f7327bf7f138da7c66336336851ba8e5482d3e20
Noodles <adrianalexis624@gmail.com>
2024-05-29 17:20:42 -0500

    Added button to adjust the size of the grid and made style changes
    The added button asks user for a size input and then changes the grid accordingly. Also added some new style changes to css and html

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
 // define grid size
 let gridSize = 16;
-function createGrid(){
+function createGrid(size){
 
     // get the container element for the grid
     const gridContainer = document.querySelector("#grid");
 
     // create the grid:
     gridContainer.setAttribute("style", `grid-template-columns: repeat(${gridSize}, 1fr);`)
-    for (let i=0; i<gridSize; i++){
-        for (let j=0; j<gridSize; j++){
+    for (let i=0; i<size; i++){
+        for (let j=0; j<size; j++){
             const gridItem = document.createElement("div");
             gridItem.className = "grid-item";
-            gridItem.setAttribute("style", "background-color: grey; outline: dotted purple;")
+            gridItem.setAttribute("style", "background-color: #ffffff;")
             
             // add color-changing event to the new grid-item
             // gridItem.addEventListener("mouseover", (e) => {
@@ -22,7 +22,7 @@ function createGrid(){
             /* either method works (top or bottom) */
 
             gridItem.addEventListener("mouseover", () => {
-                gridItem.setAttribute("style", "background-color: blue;");
+                gridItem.setAttribute("style", `background-color: rgb(${getRandomRGB()}, ${getRandomRGB()}, ${getRandomRGB()});`);
             });
 
             gridContainer.appendChild(gridItem);
@@ -30,4 +30,28 @@ function createGrid(){
     }
 }
 
-createGrid();
+function getRandomRGB(){
+    // Returns a random integer from 0 to 255:
+    return Math.floor(Math.random() * 256);
+}
+
+const adjustGrid = document.querySelector(".grid-adjuster");
+adjustGrid.addEventListener("click", () => {
+    let newSize = prompt("Enter new grid size: ");  // prompting for user input
+
+    const gridContainer = document.querySelector("#grid");  // getting refrence to the grid
+
+    // delete the current grid that we have
+    for (let i=0; i<gridSize; i++){
+        for (let j=0; j<gridSize; j++){
+            const tempNode = document.querySelector(".grid-item")
+            gridContainer.removeChild(tempNode);
+        }
+    }
+
+    // update the grid size to the new and create a new grid
+    gridSize = newSize
+    createGrid(gridSize);
+});
+
+createGrid(gridSize);
